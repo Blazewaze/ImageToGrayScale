e71f495da469c02338cba037cedc05b34edc0e61
Blazewaze <144161984+Blazewaze@users.noreply.github.com>
2024-09-21 21:30:24 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String args[]){
         Read read = new Read();
 
-        read.ConvertImage("C:\\Users\\blaze\\Pictures\\Camera Roll\\WIN_20240921_20_43_40_Pro.jpg");
+        read.ConvertImage("pathtoyourImage");
 
 
     }
